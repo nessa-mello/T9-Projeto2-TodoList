5af0cdb514c65e1022c4ad81ab26f85ffa15b6d1
Vanessa Mello <nessa_comex@hotmail.com>
2020-04-29 16:55:41 -0300

    Drag &  Drop finalizado

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', function () {
     const botao = document.querySelector('#todoSubmit')
     const botaoMarcarTodos = document.querySelector('#todoMarcarTodos')
     const botaoRemoverTodos = document.querySelector('#todoRemoverTodos')
+    let dragging = ''
     // constante botão para chamar o botão no JS. 
 
     botao.addEventListener('click', inserirItem)
@@ -21,9 +22,12 @@ window.addEventListener('DOMContentLoaded', function () {
 
             const div = document.createElement('div')
             // Criamos uma div, para inserir botão e marcação
+            div.classList.add('divDeRemocao')
+            // Aqui criamos uma class para nossa div, para que possamos remover apenas o botão e itens do botão "RemoverTodos".
 
             const removeBotao = document.createElement('button')
             // Criamos elemento botão.
+
             //----------------------------------------------------------
             // Inserções gerais - adicionado
             item.innerText = input.value;
@@ -50,16 +54,63 @@ window.addEventListener('DOMContentLoaded', function () {
 
             item.addEventListener('click', function () {
                 // regra para click no texto do botão
-                item.style.textDecoration = 'line-through'
+                // item.style.textDecoration = 'line-through'
+                if (item.style.textDecoration == 'line-through') {
+                    item.style.textDecoration = 'none'
+                } else {
+                    item.style.textDecoration = 'line-through'
+                }
             })
-            // cada vez que o input mudar, irá aparecer o Item
-        }
-        //evento para excluir todas as tarefas
-    }
-        botao.addEventListener('click', function () {
-            //adicionando a função change para quando ele mudar o botão ficar vazio
-        document.querySelector('input').value = '';
-        })
-    
 
+            //------------------------------------------------------------------
+                                //Inicio Drag & Drop
+
+            item.setAttribute('draggable', 'true');
+            // Atribuindo o draggable para os meus itens
+
+            lista.addEventListener('dragstart', function (e) {
+                dragging = e.target.closest('.divDeRemocao')
+            })
+            // 
+
+            lista.addEventListener('dragover', function (e) {
+                e.preventDefault()
+
+                const node = e.target.closest('.divDeRemocao')
+
+                this.insertBefore(dragging, node)
+            })
+
+            lista.addEventListener('dragend', function (e) {
+                dragging = null
+            })
+        }
+    }
+
+
+    //evento para excluir todas as tarefas
+
+    botao.addEventListener('click', function () {
+        //adicionando a função change para quando ele mudar o botão ficar vazio
+        document.querySelector('input').value = '';
+    })
+
+
+    todoMarcarTodos.addEventListener('click', function () {
+        //Quando eu clicar no botão selecionado eu quero que ele pegue todos os itens da lista e percorra essa lista e risque todos os itens
+
+        const li = document.querySelectorAll('li')
+        // O querySelectorAll faz com que eu percorra todos os itens da lista
+        for (let i = 0; i < li.length; i++) {
+            li[i].style.textDecoration = 'line-through'
+        }
+    })
+    todoRemoverTodos.addEventListener('click', function () {
+        //QUando eu clicar no botão indicado, eu quero que ele pegue minha div e os dois itens que criei com um clique.
+        const div = document.querySelectorAll('.divDeRemocao')
+
+        for (let i = 0; i < div.length; i++) {
+            div[i].style.display = 'none'
+        }
+    })
 })
